d17221da2576080ed20abe929e0b0fc84a8a4fa1
SaiVenkat12 <gsaiv1001@gmail.com>
2023-02-12 21:59:20 +0530

    [Sai Venkat]Added - A Simple Linked List

---
+++ CustomLinkedList/CustomLinkedList/CustomLinkedlist.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList
+{
+    internal class CustomLinkedlist
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+                Console.WriteLine("{0} inserted into the LinkedList", head.data);
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+                Console.WriteLine("{0} inserted into the LinkedList", newNode.data);
+            }
+        }
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is Empty");
+            }
+            else
+            {
+                Node temp = head;
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ CustomLinkedList/CustomLinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List");
+            CustomLinkedlist list = new CustomLinkedlist();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
